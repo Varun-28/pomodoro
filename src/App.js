228db73899db9074e55ac5b3e187c6822db804db
Varun Verma <28varun28pp@gmail.com>
2022-05-14 23:52:53 +0530

    done initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home/Home.jsx";
+import { SingleTask } from "./pages/singleTask/SingleTask.jsx";
+import "./App.css";
+import { Footer } from "./components/Components.jsx";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App min-h-screen flex flex-col">
+      <main className="grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/task/:taskId" element={<SingleTask />} />
+        </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ src/pages/singleTask/SingleTask.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./singleTask.css";
+
+function SingleTask() {
+  return (
+    <div>SingleTask</div>
+  )
+}
+
+export {SingleTask};
